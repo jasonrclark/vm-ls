e48befc12e8206404e6b1749040b3325555af749
Samruddhi Khandale <samruddhikhandale@github.com>
2024-04-04 18:14:14 +0000

    Use latest go version

--- .devcontainer/installGoPLS.sh
+++ .devcontainer/installGoPLS.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-apt-get update
-apt-get -yq install net-tools
+# Pinning go and gopls version to ensure compatibility
+GO_VERSION="1.22.0"
+GO_PLS_VERSION="v0.15.2"
 
-GO_VERSION=1.18.1
-curl -O https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+# Download and extract Go
+curl -sO https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
 tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+rm -rf go${GO_VERSION}.linux-amd64.tar.gz
 
+# Set environment variables (required for installing go modules)
 export GOPATH=/go
 export GOCACHE=$GOPATH/cache
 
 # Create a temporary directory for the Go module
 temp_dir=$(mktemp -d)
-
-# Change to the temporary directory
 cd "$temp_dir"
-
-# Initialize a new Go module
 /usr/local/go/bin/go mod init temp
 
 # Install gopls
-go install golang.org/x/tools/gopls@v0.14.2
-# Install gopls version which is compatible with go
-/usr/local/go/bin/go install golang.org/x/tools/gopls@v0.14.2
+/usr/local/go/bin/go install golang.org/x/tools/gopls@${GO_PLS_VERSION}
 
-# Change back to the original directory
+# Clean up
 cd -
-
-# Remove the temporary directory
 rm -rf "$temp_dir"
-
-echo "Go and gopls installed successfully."
