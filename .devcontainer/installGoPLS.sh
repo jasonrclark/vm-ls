9115e28ed6a7830692dd0739f76cc81bd9b36b8d
Samruddhi Khandale <samruddhikhandale@github.com>
2024-04-04 10:13:27 -0700

    sync with wrapper

--- .devcontainer/installGoPLS.sh
+++ .devcontainer/installGoPLS.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 apt-get update
-apt-get -yq install golang
+apt-get -yq install net-tools
+
+GO_VERSION=1.18.1
+curl -O https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
+tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
+
 export GOPATH=/go
 export GOCACHE=$GOPATH/cache
 
@@ -12,13 +17,17 @@ temp_dir=$(mktemp -d)
 cd "$temp_dir"
 
 # Initialize a new Go module
-go mod init temp
+/usr/local/go/bin/go mod init temp
 
 # Install gopls
-go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/gopls@v0.14.2
+# Install gopls version which is compatible with go
+/usr/local/go/bin/go install golang.org/x/tools/gopls@v0.14.2
 
 # Change back to the original directory
 cd -
 
 # Remove the temporary directory
 rm -rf "$temp_dir"
+
+echo "Go and gopls installed successfully."
